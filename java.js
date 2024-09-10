301c727a25069e14caea605ecedb78c00565f8c8
Azure-K <bernardotfarah@gmail.com>
2024-09-10 15:33:42 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,6 @@
+let button = document.getElementById('button');
+button.onclick = function(){
+    let number1 = document.getElementById('number1');
+    let signal = document.getElementById('signal');
+    let number2 = document.getElementById('number2');
+}
