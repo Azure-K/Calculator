5726da1c1dd7e7e5a92ad33c79afd2f1856f6e13
Azure-K <bernardotfarah@gmail.com>
2024-09-17 15:33:10 -0300

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,19 @@
-let button = document.getElementById('button');
-button.onclick = function(){
-    let number1 = document.getElementById('number1');
-    let signal = document.getElementById('signal');
-    let number2 = document.getElementById('number2');
+document.getElementById('button').onclick = function(){
+    let number1 = Number(document.getElementById('number1').value);
+    let signal = document.getElementById('signal').value.trim();
+    let number2 = Number(document.getElementById('number2').value);
+
+    if (signal == "+"){
+        let resultado = number1 + number2;
+        alert(resultado);
+    }else if (signal == "-"){
+        let resultado = number1 - number2;
+        alert(resultado);
+    }else if (signal == "*"){
+        let resultado = number1 * number2;
+        alert(resultado);
+    }else if (signal == "/"){
+        let resultado = number1 / number2;
+        alert(resultado);
+    }
 }
